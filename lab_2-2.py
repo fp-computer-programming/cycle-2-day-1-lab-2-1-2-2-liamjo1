8985ddbdd2f62eecc33c543aa3e9af274d943a4d
p24lohara <p24lohara@fairfieldprep.org>
2023-09-23 09:30:10 -0400

    Cycle 2 Day 1 Lab 2-2

---
+++ lab_2-2.py
@@ -0,0 +1,15 @@
+# Liam O'Hara   9/23/2023
+
+a = 3
+b = a
+
+print(b)
+print(a)
+# a = 3, b = 3 because a = 3 and a = b
+
+a = 7
+
+print(a)
+print(b)
+# a = 7, b = 3 becuause a was reasignned to 7 while b remained the same
+
