69e866a654fc8755538c9b67a75ec12bca869781
p24lohara <p24lohara@fairfieldprep.org>
2023-09-23 09:22:05 -0400

    Cycle 2 Day 1 Lab 2-1

---
+++ lab_2-1.py
@@ -0,0 +1,9 @@
+# Liam O'Hara 9/23/2023
+#Input the following into the Python interpreter and see what the output is. Be mindful of spaces. Each expression needs to be visible in your final code.
+
+
+123             +321        -321
+20+20          2020 - 4    20 + 20
+2 * 4 * 6 * 8	9 / 5		9 // 5
+13/0	        3 % 10	    3 % 10
+2 ** 3		    2 * 2 * 2	   0/0
